2fedd497563b0649554b150eb7b09ada9b2f4228
jpatkinson-rpi <jpatkinson@jpatkinson.plus.com>
2025-02-02 12:06:52 +0000

    IRQ test code

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# msf-timecode-pico
-# msf-timecode-pico
+# MSF Radio Time Signal Decoder for RPi PICO
+#
+# https://en.wikipedia.org/wiki/Time_from_NPL_(MSF)
+#
+# Data Format: http://www.npl.co.uk/upload/pdf/MSF_Time_Date_Code.pdf
+#
+# This is work-in-progress
+
--- msf-decode.py
+++ msf-decode.py
@@ -1,5 +1,11 @@
 #-------------------------------------------------------------------
-# This script decodes MSF timecode 60KHz signal for RPI PICO
+# MSF Radio Time Signal Decoder for RPi PICO
+#
+# https://en.wikipedia.org/wiki/Time_from_NPL_(MSF)
+#
+# Data Format: http://www.npl.co.uk/upload/pdf/MSF_Time_Date_Code.pdf
+#
+# This is work-in-progress
 #-------------------------------------------------------------------
 
 from machine import Pin
@@ -50,6 +56,7 @@ if __name__ == "__main__":
     #pin_button.irq(trigger=Pin.IRQ_RISING,handler=button_isr)
     while True:
         while not(irq_end):
+# initial test code for IRQ timing 
             sleep(0.1)
             pin_led.value(1)
             sleep(0.1)
